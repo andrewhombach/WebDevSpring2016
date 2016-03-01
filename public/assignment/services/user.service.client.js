6d9d548807c97d6b4c6bdf15dbee579e336bf118
andrewhombach <andrew.hombach@gmail.com>
2016-02-29 21:55:14 -0500

    added userservice functionality for profile and register

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -1,54 +1,81 @@
-(function() {
+(function () {
     angular
         .module("FormBuilderApp")
         .factory("UserService", UserService);
 
     function UserService() {
-        var currentUsers = [];
-        currentUsers = [
-            {	"_id":123, "firstName":"Alice",            "lastName":"Wonderland",
-                "username":"alice",  "password":"alice",   "roles": ["student"]		},
-            {	"_id":234, "firstName":"Bob",              "lastName":"Hope",
-                "username":"bob",    "password":"bob",     "roles": ["admin"]		},
-            {	"_id":345, "firstName":"Charlie",          "lastName":"Brown",
-                "username":"charlie","password":"charlie", "roles": ["faculty"]		},
-            {	"_id":456, "firstName":"Dan",              "lastName":"Craig",
-                "username":"dan",    "password":"dan",     "roles": ["faculty", "admin"]},
-            {	"_id":567, "firstName":"Edward",           "lastName":"Norton",
-                "username":"ed",     "password":"ed",      "roles": ["student"]		}
-        ];
-
-        var api = {
-            findUserByCredentials: findUserByCredentials,
-            findAllUsers: findAllUsers,
+        var model = {
+            users: [
+                {
+                    "_id": 123, "firstName": "Alice", "lastName": "Wonderland",
+                    "username": "alice", "password": "alice", "roles": ["student"]
+                },
+                {
+                    "_id": 234, "firstName": "Bob", "lastName": "Hope",
+                    "username": "bob", "password": "bob", "roles": ["admin"]
+                },
+                {
+                    "_id": 345, "firstName": "Charlie", "lastName": "Brown",
+                    "username": "charlie", "password": "charlie", "roles": ["faculty"]
+                },
+                {
+                    "_id": 456, "firstName": "Dan", "lastName": "Craig",
+                    "username": "dan", "password": "dan", "roles": ["faculty", "admin"]
+                },
+                {
+                    "_id": 567, "firstName": "Edward", "lastName": "Norton",
+                    "username": "ed", "password": "ed", "roles": ["student"]
+                }
+            ],
             createUser: createUser,
             deleteUserById: deleteUserById,
+            findAllUsers: findAllUsers,
+            findUserByCredentials: findUserByCredentials,
             updateUser: updateUser
         };
 
-        function findUserByCredentials(username, password, callback) {
-            for (var user in currentUsers) {
-                if (currentUsers[user].username === username && currentUsers[user].password === password) {
-                    callback(currentUsers[user]);
+        return model;
+
+        function findUserByCredentials (username, password, callback) {
+            for (var user in users) {
+                if (users[user].username === username && users[user].password === password) {
+                    callback(users[user]);
                 }
             }
         }
 
-        function findAllUsers(callback) {
-            callback(currentUsers)
+        function findAllUsers (callback) {
+            callback(users)
         }
 
-        function deleteUserById(userId, callback){
-            for (var user in currentUsers) {
-                if (currentUsers[user]._id === userId) {
-                    currentUsers[user].splice(user,1);
-                    callback(currentUsers);
+        function deleteUserById (userId, callback) {
+            for (var user in users) {
+                if (users[user]._id === userId) {
+                    users[user].splice(user, 1);
+                    callback(users);
                 }
             }
         }
 
-        function createUser(user, callback) {
-            var newUser = { "_id":(new Date).getTime(), "firstname":user.firstName}
+        function createUser (user, callback) {
+            var newUser = {
+                username: user.username,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                password: user.password
+            };
+            model.users.push(newUser);
+            callback(newUser);
+        }
+
+        function updateUser (userId, user, callback) {
+            for (var u in model.users) {
+                if (model.users[u]._id === userId) {
+                    model.users[u] = user;
+                    callback(model.users[u]);
+                }
+            }
         }
     }
 })();
--- public/assignment/views/users/profile.controller.js
+++ public/assignment/views/users/profile.controller.js
@@ -3,7 +3,16 @@
         .module("FormBuilderApp")
         .controller("ProfileController", ProfileController);
 
-    function ProfileController() {
+    function ProfileController ($scope, $rootScope, UserService) {
+        $scope.update = update;
+
+        function update (user) {
+            var callback = function (response) {
+                $rootScope.cUser = response;
+            };
+            UserService.updateUser(user._id, user, callback);
+            console.log($rootScope.cUser);
+         }
 
     }
 
