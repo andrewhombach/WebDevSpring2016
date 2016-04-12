3be68cd89039f84c7b060e038999bb1120b729a0
andrewhombach <andrew.hombach@gmail.com>
2016-04-12 05:41:39 -0400

    fix openshift 2

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var connectionString = 'mongodb://127.0.0.1:27017/webdev-db/';
-var db = mongoose.connect(connectionString);
 
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
@@ -21,6 +20,9 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
             process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
             process.env.OPENSHIFT_APP_NAME;
 }
+
+var db = mongoose.connect(connectionString);
+
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
